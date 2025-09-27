6d6130e8f651c64259c25f95dfd31d0caa6c8c4d
barbariand <cindy@simd.me>
2025-09-27 22:52:18 +0200

    fix: updating to get more working now partially complete

--- compile.sh
+++ compile.sh
@@ -1,3 +1,19 @@
-#! /usr/bin/env bash
-latexmk -f -pdf -outdir=./out/aux/ -out2dir=./out/pdf/ main.tex
+#!/usr/bin/env bash
+
+# This script compiles main.tex and cleans up auxiliary files.
+
+# Clean the output directories to ensure a fresh build
+rm -f ./out/pdf/*
+rm -f ./out/aux/*
+rm -f ./latex.log
+
+latexmk -pdf -f \
+  -interaction=nonstopmode \
+  -outdir=./out/aux \
+  -out2dir=./out/pdf \
+  main.tex
+
+# Move the log file to a cleaner name and location for inspection.
 mv ./out/aux/main.log ./latex.log
+
+echo "Compilation finished. PDF is in ./out/pdf/main.pdf"
