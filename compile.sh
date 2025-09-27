63703307963b65d4773c4fa0b071fb4b360b3d8b
barbariand <cindy@simd.me>
2025-09-27 17:13:45 +0200

    working

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-latexmk -pdf -outdir=./out/aux/ -out2dir=./out/pdf/ main.tex
+latexmk -f -pdf -outdir=./out/aux/ -out2dir=./out/pdf/ main.tex
 mv ./out/aux/main.log ./latex.log
