89fad9c7a344be88adc2cee28d23568478693fc9
barbariand <cindy@simd.me>
2025-09-27 10:34:47 +0200

    Init: pushing a starter to base it out of

---
+++ flake.nix
@@ -0,0 +1,22 @@
+# This file is to easily install latex dependancies to compile localy
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = with pkgs;
+        mkShell {
+          packages = with pkgs; [texliveFull tex-fmt];
+          shellHook = ''
+          '';
+        };
+    });
+}
